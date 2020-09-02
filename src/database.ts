6525f0e6877c23595ebd764b733455e4fc5932b0
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-09-02 02:06:00 -0500

    crud con firebase completo ts

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
 const credenciales = require("../credenciales.json");
 admin.initializeApp({
-  credential: admin.credential.cert(credenciales),
+  // credential: admin.credential.cert(credenciales),
+  //https://cloud.google.com/docs/authentication/getting-started#windows
+  credential: admin.credential.applicationDefault(),
   databaseURL: "https://fb-api-86cd1.firebaseio.com",
 });
 
